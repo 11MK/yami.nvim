a02bfde39c233c82ce74ade00f6c01d6673f3ead
Maximus1198 <scrappy11112222@hotmail.com>
2023-10-12 20:18:33 +0000

    remove styles

--- lua/yami/config.lua
+++ lua/yami/config.lua
@@ -2,7 +2,7 @@ local config = {}
 
 local default_config = {
 	-- Main options --
-	style = "dark", -- choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	-- style = "dark", -- choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 	transparent = false, -- don't set background
 	term_colors = true, -- if true enable the terminal
 	ending_tildes = false, -- show the end-of-buffer tildes
