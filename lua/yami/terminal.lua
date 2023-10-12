29a61b997e1c161efb4b4ebb472fb31d25edbde1
Maximus1198 <scrappy11112222@hotmail.com>
2023-10-12 20:28:51 +0000

    [fix] source palette

--- lua/yami/terminal.lua
+++ lua/yami/terminal.lua
@@ -1,32 +1,32 @@
 local M = {}
 local config = require('yami.config')
-local c = require 'yami.colors'
+local p = require("tokyodark.palette")
 
 function M.setup()
     if not config.term_colors then return end
-    vim.g.terminal_color_0 = c.black
-    vim.g.terminal_color_8 = c.bg2
+    vim.g.terminal_color_0 = p.black
+    vim.g.terminal_color_8 = p.bg2
 
-    vim.g.terminal_color_7 = c.fg
-    vim.g.terminal_color_15 = c.fg
+    vim.g.terminal_color_7 = p.fg
+    vim.g.terminal_color_15 = p.fg
 
-    vim.g.terminal_color_1 = c.red
-    vim.g.terminal_color_9 = c.red
+    vim.g.terminal_color_1 = p.red
+    vim.g.terminal_color_9 = p.red
 
-    vim.g.terminal_color_2 = c.green
-    vim.g.terminal_color_10 = c.green
+    vim.g.terminal_color_2 = p.green
+    vim.g.terminal_color_10 = p.green
 
-    vim.g.terminal_color_3 = c.yellow
-    vim.g.terminal_color_11 = c.yellow
+    vim.g.terminal_color_3 = p.yellow
+    vim.g.terminal_color_11 = p.yellow
 
-    vim.g.terminal_color_4 = c.blue
-    vim.g.terminal_color_12 = c.blue
+    vim.g.terminal_color_4 = p.blue
+    vim.g.terminal_color_12 = p.blue
 
-    vim.g.terminal_color_5 = c.purple
-    vim.g.terminal_color_13 = c.purple
+    vim.g.terminal_color_5 = p.purple
+    vim.g.terminal_color_13 = p.purple
 
-    vim.g.terminal_color_6 = c.cyan
-    vim.g.terminal_color_14 = c.cyan
+    vim.g.terminal_color_6 = p.cyan
+    vim.g.terminal_color_14 = p.cyan
 end
 
 return M
