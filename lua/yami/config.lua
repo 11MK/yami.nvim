3e4ed78c2906ad467c0c2db0709e308413ebfc21
Maximus1198 <scrappy11112222@hotmail.com>
2023-10-12 20:05:38 +0000

    first commit

---
+++ lua/yami/config.lua
@@ -0,0 +1,33 @@
+local config = {}
+
+local default_config = {
+	-- Main options --
+	style = "dark", -- choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- don't set background
+	term_colors = true, -- if true enable the terminal
+	ending_tildes = false, -- show the end-of-buffer tildes
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+	-- Changing Formats --
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
+	},
+}
+
+function config.setup(opts)
+	for k, v in pairs(opts or {}) do
+		if type(v) == "table" then
+			config[k] = {}
+			for kk, vv in pairs(v) do
+				config[k][kk] = vv
+			end
+		else
+			config[k] = v
+		end
+	end
+end
+
+return setmetatable(config, { __index = default_config })
